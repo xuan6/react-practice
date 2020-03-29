f6ecfffddb2605363985098d911779cd32c77b4f
Xuan liu <xuanliu@uw.edu>
2020-03-29 10:36:29 -0700

    adding card component and search filed

--- monsters-rolodex/src/components/card/card.componet.jsx
+++ monsters-rolodex/src/components/card/card.componet.jsx
@@ -6,7 +6,7 @@ export const Card = props => (
         <img
         alt="monster"
         number={props.monster.id}
-        src= {'https://robohash.org/${number}?set=set2&size=180x180'} />
+        src= {'https://robohash.org/${props.monster.id}?set=set2&size=180x180'} />
         <h2> {props.monster.name} </h2>
         <p> {props.monster.email}</p>
     </div>
