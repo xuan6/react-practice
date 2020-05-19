0eba6f7e5cc5f1ec4b02046962562c42897bfd7c
Xuan liu <xuanliu@uw.edu>
2020-05-18 20:25:33 -0700

    updating delopy info

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,26 +1,35 @@
 import React from 'react';
+import { Component } from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters:[]
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then( response => response.json())
+    .then(users => this.setState({ monsters: users }))
+  }
+
+  render(){
+    return (
+      <div className="App">
+        {this.state.monsters.map(monsters => (
+          <h1 key={monsters.id}> {monsters.name} </h1>
+          ))}  
+      </div>
+    );
+
+  };
+
 }
+
 
 export default App;
