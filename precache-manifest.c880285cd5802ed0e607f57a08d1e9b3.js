99bbd2baebb098e2df5e0b59052495cacf8eaa5c
Xuan liu <xuanliu@uw.edu>
2020-05-18 20:22:06 -0700

    Updates

---
+++ precache-manifest.c880285cd5802ed0e607f57a08d1e9b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5d7473386e1f0f24337a7aa31a93bfe",
+    "url": "/react-practice/index.html"
+  },
+  {
+    "revision": "53c140028fde1d862c7b",
+    "url": "/react-practice/static/css/main.ed02f164.chunk.css"
+  },
+  {
+    "revision": "b235cfb64ee3d31b3962",
+    "url": "/react-practice/static/js/2.dfd295bd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-practice/static/js/2.dfd295bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "53c140028fde1d862c7b",
+    "url": "/react-practice/static/js/main.f50d0066.chunk.js"
+  },
+  {
+    "revision": "54d6348fe3e16d97f8bc",
+    "url": "/react-practice/static/js/runtime-main.0c35214c.js"
+  }
+]);
