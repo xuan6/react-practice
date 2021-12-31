6dffe79b51d56bd02b49fd88d0aef8d0e7a6b2d9
Xuan liu <xuanliu@uw.edu>
2021-12-30 23:18:45 -0800

    adding a toggle to show more or less of the description text finishing project 2

--- 02-tours/src/Tour.js
+++ 02-tours/src/Tour.js
@@ -1,8 +1,21 @@
 import React, { useState } from 'react';
 
 const Tour = ({id, name, info, price, image, onDelete}) => {
-    const onClick =()=>{
+    const deleteTour =()=>{
         onDelete(id)   
+    }
+
+    const [btnText, setBtnText] = useState('show more')
+    const [textTruncation, seTextTruncation] = useState(true)
+
+    const toggleInfo =()=>{
+        if (btnText === 'show more'){
+            setBtnText('show less')
+            seTextTruncation(false)
+        }else{
+            setBtnText('show more')
+            seTextTruncation(true)
+        }
     }
 
     return (
@@ -11,10 +24,10 @@ const Tour = ({id, name, info, price, image, onDelete}) => {
         <footer>
             <div className='tour-info'>
                 <h4>{name}</h4>
-                <h4 className='tour-price'>{price}</h4>
+                <h4 className='tour-price'>${price}</h4>
             </div>
-            <p>{info}</p>
-            <button className='delete-btn' onClick={onClick}>not interested</button>
+            <p>{textTruncation? `${info.substring(0,200)}...`:`${info}`}<button onClick={toggleInfo}>{btnText}</button></p>
+            <button className='delete-btn' onClick={deleteTour}>not interested</button>
         </footer>
         
       </article>
