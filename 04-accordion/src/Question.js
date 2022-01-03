42cb4631c242b6924d2b5590f4386bc79a3bac66
Xuan liu <xuanliu@uw.edu>
2022-01-02 23:42:29 -0800

    updating css and html to align with UI example; finishing project 4

--- 04-accordion/src/Question.js
+++ 04-accordion/src/Question.js
@@ -1,17 +1,28 @@
 import React, { useState } from 'react';
+import {FaPlus, FaMinus} from "react-icons/fa"; 
 function Question({title, info}) {
   //toggle answer visibility
   const [visibility, setVisibility] = useState(false)
+  const [icon, setIcon] = useState(<FaPlus/>)
+
   const onClick = () =>{
     setVisibility(!visibility)
+    if (visibility){
+      setIcon(<FaPlus/>)
+    }else{
+      setIcon(<FaMinus/>)
+    }
   }
 
+
+    
+  
   return (
     <div className='question'>
-      <div className='header'>
+      <header>
         <h4>{title}</h4>
-        <button className='btn' onClick={onClick}></button>
-      </div>
+        <button className='btn' onClick={onClick}>{icon}</button>
+      </header>
       {visibility?<p>{info}</p>:''}
     </div>
   );
