8872734bd72b9af9399be6f042e8591790f32696
Xuan liu <xuanliu@uw.edu>
2022-01-02 23:19:51 -0800

    adding toggle button

--- 04-accordion/src/App.js
+++ 04-accordion/src/App.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import data from './data';
-import SingleQuestion from './Question';
 import Questions from './Questions';
 function App() {
 
--- 04-accordion/src/Question.js
+++ 04-accordion/src/Question.js
@@ -1,12 +1,18 @@
 import React, { useState } from 'react';
 function Question({title, info}) {
+  //toggle answer visibility
+  const [visibility, setVisibility] = useState(false)
+  const onClick = () =>{
+    setVisibility(!visibility)
+  }
+
   return (
     <div className='question'>
       <div className='header'>
         <h4>{title}</h4>
-        <button className='btn'></button>
+        <button className='btn' onClick={onClick}></button>
       </div>
-      <p>{info}</p>
+      {visibility?<p>{info}</p>:''}
     </div>
   );
 }
