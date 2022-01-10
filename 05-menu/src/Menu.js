906a3cb353c1576ad0558408b390c025e30075d4
Xuan liu <xuanliu@uw.edu>
2022-01-09 22:03:45 -0800

    displaying all items;

--- 05-menu/src/Menu.js
+++ 05-menu/src/Menu.js
@@ -4,11 +4,13 @@ const Menu = ({image, title, category, price, info}) => {
   return (
       <div className='menu-item'>
         <img className='photo' src={image} alt={title}/>
-        <header className='item-info'>
-            <h4>{title}</h4>
-            <h4>{price}</h4>
-        </header>
-        <p className='item-text'>{info}</p>
+        <div className='item-info'>
+            <header >
+                <h4>{title}</h4>
+                <h4>{price}</h4>
+            </header>
+            <p className='item-text'>{info}</p>
+        </div>
       </div>
       );
 };
