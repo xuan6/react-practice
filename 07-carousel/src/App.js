61b7a55621d4fab9fdb5cd37128ebd13844c0fe6
Xuan liu <xuanliu@uw.edu>
2022-01-16 11:43:19 -0800

    finishing carousel slideshow

--- 07-carousel/src/App.js
+++ 07-carousel/src/App.js
@@ -1,16 +1,36 @@
 import React, { useState, useEffect } from 'react';
-
-
 import data from './data';
 import Slide from './Slide';
+
 function App() {
+  //the index of the slide to be dislplayed
+  const[index, setIndex] = useState(0)
+  let count = data.length
+
+  //click the next button will slide to the next page
+  const goToNext = (currentIndex) => {
+    if (currentIndex===count-1){
+      setIndex(0)
+    }else{
+      setIndex(index+1)
+    }
+  }
+
+  //click the prev button will slide to the neprevxt page
+  const goToPrev = (currentIndex) => {
+    if (currentIndex===0){
+      setIndex(count-1)
+    }else{
+      setIndex(index-1)
+    }
+  }
   
   return (
     <main className='section'>
       <div className='title'>
         <h2><span>/</span>Reviews</h2>
       </div>
-      <Slide data={data}/>
+      <Slide data={data} targetedIndex={index} prev={goToPrev} next={goToNext}/>
     </main>
   );
 }
--- 07-carousel/src/Slide.js
+++ 07-carousel/src/Slide.js
@@ -1,21 +1,31 @@
 import { FaQuoteRight } from 'react-icons/fa';
 import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
 
-const Slide = ({data}) => {
+const Slide = ({data, targetedIndex, prev, next}) => {
+    
     return (
-        <article className='section-center'>
-        {data.map((people)=>(
-        <div key={people.id}>
-        <FiChevronLeft className='prev'/>
-          <img className='person-img' src={people.image} alt={people.name}/>
-          <h4>{people.name}</h4>
-          <p>{people.title}</p>
-          <p className='text'>{people.quote}</p>
-          <FaQuoteRight className='icon' />
-          <FiChevronRight className='next' />
-        </div>
-        ))}
-      </article>
+        <div className='section-center'>
+        {data.map((people, index) => {
+                let position = 'nextSlide';
+                if(index===targetedIndex){
+                    position = 'activeSlide'
+                }
+                if (index===targetedIndex-1 || (targetedIndex===0 && index===data.length-1)){
+                    position= 'lastSlide'
+                }
+                return (
+                    <article key={people.id} className={position}>
+                        <img className='person-img' src={people.image} alt={people.name}/>
+                        <h4>{people.name}</h4>
+                        <p>{people.title}</p>
+                        <p className='text'>{people.quote}</p>
+                        <FaQuoteRight className='icon' />
+                    </article>
+                )
+            })}
+        <button className='prev' onClick={()=>prev(targetedIndex)} ><FiChevronLeft /></button>
+        <button className='next' onClick={()=>next(targetedIndex)} ><FiChevronRight /></button>
+      </div>
     )
 }
 
