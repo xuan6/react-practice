33f33ad252ccf1a87ff99a18a458a79a1d21170c
Xuan liu <xuanliu@uw.edu>
2022-02-26 18:15:28 -0800

    adding add and clear all function

--- 10-grocery-list/src/App.js
+++ 10-grocery-list/src/App.js
@@ -3,7 +3,47 @@ import List from './List'
 import Alert from './Alert'
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const[list, setList] = useState([])
+  const[item, setItem] = useState('')
+  
+  const addItem =(e)=> {
+    e.preventDefault();
+
+    //add new item
+    list.push(item);
+    setList(list);
+    console.log(list);
+
+    //rest form
+    setItem('');
+  }
+
+  const ListView =()=> {
+    return <div className='grocery-container'>
+    {list.map((item, index)=>(<List key={index} item={item}/>))}
+    </div>
+  }
+
+  return <section className='section-center'>
+  <Alert/>
+  <div className='grocery-form'>
+    <h3>Grocery List</h3>
+    <form className='form-control' onSubmit={addItem}>
+      <input
+        className='grocery'
+        type='text'
+        value={item}
+        placeholder='e.g. apples'
+        onChange={(e)=>setItem(e.target.value)}/>
+      <button type='submit' className='submit-btn'>add</button>
+    </form>
+  </div>
+  {list.length>0 && <div className='grocery-container'>
+  {list.map((item, index)=>(<List key={index} item={item}/>))}
+  </div>}
+  {list.length>0 && <button className='clear-btn' onClick={(e)=>setList([])}>clear</button>}
+</section>
+  
 }
 
 export default App
--- 10-grocery-list/src/List.js
+++ 10-grocery-list/src/List.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = () => {
-  return <h2>list component</h2>
+const List = ({item}) => {
+  return <div className='grocery-item'>
+  <p className='title'>{item}</p>
+  <div className='btn-group'>
+    <button className='edit-btn'><FaEdit/></button>
+    <button className='delete-btn'><FaTrash/></button>
+  </div>
+  </div>
 }
 
 export default List
