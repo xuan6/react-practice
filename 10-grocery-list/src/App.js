8c28e2e96297b36a149da80bc8b57ea9bf925f52
Xuan liu <xuanliu@uw.edu>
2022-02-26 18:34:14 -0800

    adding edit item function

--- 10-grocery-list/src/App.js
+++ 10-grocery-list/src/App.js
@@ -5,43 +5,60 @@ import Alert from './Alert'
 function App() {
   const[list, setList] = useState([])
   const[item, setItem] = useState('')
+  const[buttonText, setButtonText] = useState('add')
+  const[itemToEdit, setItemToEdit] = useState()
   
-  const addItem =(e)=> {
+  const onSubmit =(e)=> {
     e.preventDefault();
 
-    //add new item
-    list.push(item);
-    setList(list);
-    console.log(list);
-
+    if(buttonText==='add'){
+      //add new item
+      list.push(item);
+      setList(list);
+      console.log(list);
+    }else{
+      //edit existing item
+      list[itemToEdit]=item;
+      setList(list);
+      console.log(list);
+      setButtonText('add')
+    }
     //rest form
     setItem('');
+    
   }
 
-  const ListView =()=> {
-    return <div className='grocery-container'>
-    {list.map((item, index)=>(<List key={index} item={item}/>))}
-    </div>
+  const onEdit =(item, index)=>{
+    setItem(item);
+    setButtonText('Edit');
+    //locate item by index
+    console.log(index);
+    setItemToEdit(index);
   }
 
   return <section className='section-center'>
   <Alert/>
+  
   <div className='grocery-form'>
     <h3>Grocery List</h3>
-    <form className='form-control' onSubmit={addItem}>
+    <form className='form-control' onSubmit={onSubmit}>
       <input
         className='grocery'
         type='text'
         value={item}
         placeholder='e.g. apples'
         onChange={(e)=>setItem(e.target.value)}/>
-      <button type='submit' className='submit-btn'>add</button>
+      <button type='submit' className='submit-btn'>{buttonText}</button>
     </form>
   </div>
+
+  {/* rendering list items only when list is not empty */}
   {list.length>0 && <div className='grocery-container'>
-  {list.map((item, index)=>(<List key={index} item={item}/>))}
+  {list.map((item, index)=>(<List key={index} item={item} index={index} Edit={onEdit}/>))}
   </div>}
-  {list.length>0 && <button className='clear-btn' onClick={(e)=>setList([])}>clear</button>}
+
+  {/* rendering clear all button only when list is not empty */}
+  {list.length>0 && <button className='clear-btn' onClick={(e)=>setList([])}>clear all</button>}
 </section>
   
 }
--- 10-grocery-list/src/List.js
+++ 10-grocery-list/src/List.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import { FaEdit, FaTrash } from 'react-icons/fa'
-const List = ({item}) => {
+const List = ({item, index, Edit}) => {
   return <div className='grocery-item'>
   <p className='title'>{item}</p>
   <div className='btn-group'>
-    <button className='edit-btn'><FaEdit/></button>
+    <button className='edit-btn' onClick={()=>Edit(item, index)}><FaEdit/></button>
     <button className='delete-btn'><FaTrash/></button>
   </div>
   </div>
